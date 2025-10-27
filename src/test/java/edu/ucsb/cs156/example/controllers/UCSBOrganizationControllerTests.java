339cdd11549a7a155d56c926367bb4bf891e6477
guntash gill <guntashgill@ucsb.edu>
2025-10-27 14:55:12 -0700

    gg- ucsborganization, minor changes to test

--- src/test/java/edu/ucsb/cs156/example/controllers/UCSBOrganizationControllerTests.java
+++ src/test/java/edu/ucsb/cs156/example/controllers/UCSBOrganizationControllerTests.java
@@ -36,8 +36,6 @@ public class UCSBOrganizationControllerTests extends ControllerTestCase {
 
   @MockBean UserRepository userRepository;
 
-  // ---------------- AUTHORIZATION TESTS ----------------
-
   @Test
   public void logged_out_users_cannot_get_all() throws Exception {
     mockMvc.perform(get("/api/ucsborganizations/all")).andExpect(status().is(403));
@@ -59,8 +57,6 @@ public class UCSBOrganizationControllerTests extends ControllerTestCase {
   public void regular_users_cannot_post() throws Exception {
     mockMvc.perform(post("/api/ucsborganizations/post")).andExpect(status().is(403));
   }
-
-  // ---------------- MOCKED DB TESTS ----------------
 
   @WithMockUser(roles = {"USER"})
   @Test
@@ -96,11 +92,8 @@ public class UCSBOrganizationControllerTests extends ControllerTestCase {
   @WithMockUser(roles = {"ADMIN", "USER"})
   @Test
   public void admin_can_post_new_organization() throws Exception {
-    // Arrange
     when(ucsbOrganizationRepository.save(any(UCSBOrganization.class)))
         .thenAnswer(inv -> inv.getArgument(0));
-
-    // Act
     MvcResult response =
         mockMvc
             .perform(
@@ -113,8 +106,6 @@ public class UCSBOrganizationControllerTests extends ControllerTestCase {
                     .contentType(MediaType.APPLICATION_FORM_URLENCODED))
             .andExpect(status().isOk())
             .andReturn();
-
-    // Assert
     ArgumentCaptor<UCSBOrganization> captor = ArgumentCaptor.forClass(UCSBOrganization.class);
     verify(ucsbOrganizationRepository, times(1)).save(captor.capture());
 
