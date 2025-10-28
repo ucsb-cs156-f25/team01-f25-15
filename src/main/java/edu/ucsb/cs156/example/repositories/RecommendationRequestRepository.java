80c40c30927a1adc0995be29df5c1b66af9bebec
Tim Nguyen <tvn@ucsb.edu>
2025-10-28 14:44:54 -0700

    Create database table for Commits

--- src/main/java/edu/ucsb/cs156/example/repositories/RecommendationRequestRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/RecommendationRequestRepository.java
@@ -1,5 +1,6 @@
 package edu.ucsb.cs156.example.repositories;
 
+import edu.ucsb.cs156.example.entities.RecommendationRequest;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
